0132ebc8ddc699c534b6ae53a6c1dac3595e9cf8
Ishan Jaiswal <ishanj626@gmail.com>
2025-09-12 22:02:15 +0530

    Added markdown comprehender

--- src/app/bot_component/AiMessage.jsx
+++ src/app/bot_component/AiMessage.jsx
@@ -1,16 +1,29 @@
 import { BotMessageSquareIcon } from "lucide-react";
-import { Loader2 } from "lucide-react";
+import { Inter, Poppins } from "next/font/google";
 import React from "react";
+import ReactMarkdown from "react-markdown";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], // choose what you need
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // pick weights you need
+});
 
 export default function AI_Message({ message }) {
   return (
     <div className="flex w-full my-2">
       <div className="max-w-200 mt-1">
-        <div className="text-black self-start p-2 text-sm rounded-lg ml-1 mt-1 font-mono">
+        <div
+          className={`text-black self-start p-2 text-md rounded-lg ml-1 mt-1 ${inter.className} tracking-wide`}
+        >
           <div className="my-0.5">
             <BotMessageSquareIcon />
           </div>
-          {message}
+          <ReactMarkdown>{message}</ReactMarkdown>
         </div>
       </div>
     </div>
--- src/app/bot_component/HumanMessage.jsx
+++ src/app/bot_component/HumanMessage.jsx
@@ -5,7 +5,7 @@ export default function Human_message({ message }) {
   return (
     <div className="w-full flex justify-end">
       <div className="max-w-75 mt-1">
-        <div className="bg-zinc-300 text-black self-end p-2 text-[13px] rounded-md mt-1 mr-1 font-sans">
+        <div className="bg-zinc-300 text-black self-end p-2 text-md rounded-md mt-1 mr-1 font-sans">
           {message}
         </div>
       </div>
